2247361c8a1086246bc31faf5dc316e8bd3d65b8
Joel H <joel.hacke@gmail.com>
2021-04-01 22:06:05 -0500

    working on dropdown for pictures

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement, useState } from 'react'
 import { LocationDict } from '../Pictures/Picture.types';
 import './Header.css';
 
@@ -10,29 +10,62 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({title, pictures}) => {
   const navClassName = 'nav-item';
 
-  const content = Object.keys(pictures).map(value => {
-    const name = pictures[value].name
-    return (
-      <li
-        className="dropdown-items"
-      >
-        {name}
-      </li>)
-      
-  })
-
   return (
     <nav className="navbar">
       <ul className={"navbar-nav"}>
-        <li className={navClassName}>{title}</li>
-        <li className={navClassName}>
-          <div className="dropdown">
-            {/* need a wrapper :o */}
-            {/* {true && content} */}
-          </div>
-          Content
-        </li>
-      </ul>
+        <NavItem buttonName={'Counter'}/>
+        <NavItem buttonName={'HOME'}/>
+        <NavItem buttonName={'Content'}>
+          <DropDownMenu />
+        </NavItem>
+        </ul>
     </nav>
   );
+}
+
+
+interface NavItemProps {
+  buttonName: string;
+}
+
+export const NavItem: React.FC<NavItemProps> = ({ buttonName, children }) => {
+  const [ open, setOpen ] = useState(false);
+
+  // const content = Object.keys(pictures).map(value => {
+  //   const name = pictures[value].name
+  //   return (
+  //     <li
+  //       className="nav-item"
+  //     >
+  //       {name}
+  //     </li>)
+      
+  // })
+
+    return (
+      <li
+        className="nav-item"
+        onClick={() => setOpen(!open)}
+      >
+        {buttonName}
+        {open && children}
+      </li>
+    );
+}
+
+
+
+interface DropDownMenuProps {
+
+}
+
+export const DropDownMenu: React.FC<DropDownMenuProps> = ({children}) => {
+  const DropDownItem(children) = {
+    // handle each picture here?
+  }
+    return (
+      <div className="dropdown-item">
+        {children}
+      </div>
+    );
 }
