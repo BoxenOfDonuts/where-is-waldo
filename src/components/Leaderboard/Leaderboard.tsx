8fb858e7f25d0313cd4c5dcac72b2ca4b08d8b46
Joel H <joel.hacke@gmail.com>
2021-05-02 11:52:58 -0500

    styling updates

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -3,16 +3,19 @@ import { useScoreListener } from "../../hooks/ScoreListener";
 import { FirebaseContext } from "../Firebase";
 import { FirebaseUtil } from "../Firebase/Firebase.types";
 import { Header } from '../Header/Header';
+import './Leaderboard.css';
 
 
 const LeaderBoardItem: React.FC<any> = ({ name, score }): JSX.Element => {
   return (
     <li className="score-item">
-      <div className="names">
-        {name}
-      </div>
-      <div className="score">
-        {score}
+      <div className="leaderboard-card">
+        <div className="names">
+          {name}
+        </div>
+        <div className="score">
+          {score}
+        </div>
       </div>
     </li>
   );
@@ -24,15 +27,16 @@ const Leaderboard: React.FC = () => {
   const [ scores ] = useScoreListener(firebase);
 
   return (
-    <>
+    <div className={'leaderboard-wrapper'}>
       <Header title={"Where is Waldo"}/>
-      <ul className="leader-board">
+      <ul className="leaderboard">
+        <LeaderBoardItem key={'head'} name={'Name'} score={'Score in Seconds'} />
         {scores?.map(value => {
           const { name, score, timestamp } = value;
           return <LeaderBoardItem key={timestamp} name={name} score={score} />
         })}
       </ul>
-    </>
+    </div>
   );
 }
 
